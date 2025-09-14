7c5430aa1af25b175a5941f6e067ec92865abe26
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-09-14 10:47:11 +0300

    Extend tests

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,14 @@
-while read -r line; do
-  echo $line | jq .
-  curl --header "Content-Type: application/json" \
-       --data-raw "$line" \
-       http://localhost:8080/replaceMorphologicalAnalysis
-done < morphologicalAnalysisReplacements.jsonlines
-while read -r line; do
-  echo $line | jq .
-  curl --header "Content-Type: application/json" \
-       --data-raw "$line" \
-       http://localhost:8080/replaceTranslation
-done < translationReplacements.jsonlines
-curl localhost:8080 | grep -E 'Hirsch|Rehbock|curse|verfluchen'
+function curl_jsonlines() {
+  while read -r line; do
+    echo $line | jq .
+    curl --header "Content-Type: application/json" \
+        --data-raw "$line" \
+        http://localhost:8080/$2
+  done < $1
+  return
+}
+curl_jsonlines morphologicalAnalysisReplacements.jsonlines replaceMorphologicalAnalysis
+curl_jsonlines translationReplacements.jsonlines replaceTranslation
+curl_jsonlines stemReplacements.jsonlines replaceStem
+curl_jsonlines posReplacements.jsonlines replacePos
+curl localhost:8080 | grep -E 'Hirsch|Rehbock|curse|verfluchen|nāli|šid|naali|šidd|ḫimzatḫ|adj'
