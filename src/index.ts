6c79617dc888073714d0b547129c089e69eb9310
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-09-20 11:24:33 +0300

    Send events from the server when the lexical database is modified

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import express from 'express';
+import express, {Response} from 'express';
 import bodyParser from 'body-parser';
 import {LexicalDatabase} from './objects/lexicalDatabase.js';
 
@@ -17,6 +17,8 @@ if (fs.existsSync(dictionaryFilePath)) {
 function saveLexicalDatabase(): void {
   fs.writeFileSync(dictionaryFilePath, lexicalDatabase.toString(), 'utf8')
 }
+
+let clients: Response[] = [];
 
 // Constants
 var DEFAULT_PORT = 8080;
@@ -39,6 +41,22 @@ app.get('/', function (req, res) {
   res.send(lexicalDatabase.toJSON());
 });
 
+app.get('/getMorphologicalAnalysisReplacements', function (req, res) {
+  clients.push(res);
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders(); // flush the headers to establish SSE with client
+  const message = 'data: ' + JSON.stringify('Initial message');
+  const event = `event: replaceMorphologicalAnalysis\n${message}\n\n`;
+  res.write(event);
+  res.on('close', () => {
+    res.end();
+    clients.splice(clients.indexOf(res), 1);
+  });
+});
+
 app.post('/uploadLexicalDatabase', jsonParser, function (req, res) {
   lexicalDatabase = new LexicalDatabase(req.body);
   res.set('Access-Control-Allow-Origin', '*');
@@ -46,68 +64,91 @@ app.post('/uploadLexicalDatabase', jsonParser, function (req, res) {
   saveLexicalDatabase();
 });
 
-app.post('/replaceMorphologicalAnalysis', jsonParser, function (req, res) {
+app.post('/replaceMorphologicalAnalysis', jsonParser, function (req, res, next) {
   const {transcriptions, origin, target} = req.body;
   lexicalDatabase.replaceMorphologicalAnalysis(transcriptions, origin, target);
   res.set('Access-Control-Allow-Origin', '*');
   res.sendStatus(204);
   saveLexicalDatabase();
+  next();
 });
 
-app.post('/replaceStem', jsonParser, function (req, res) {
+app.post('/replaceStem', jsonParser, function (req, res, next) {
   const {oldStem, newStem, pos, translation} = req.body;
   lexicalDatabase.changeStem(oldStem, newStem, pos, translation);
   res.set('Access-Control-Allow-Origin', '*');
   res.sendStatus(204);
   saveLexicalDatabase();
+  next();
 });
 
-app.post('/replacePos', jsonParser, function (req, res) {
+app.post('/replacePos', jsonParser, function (req, res, next) {
   const {stem, oldPos, newPos, translation} = req.body;
   lexicalDatabase.changePos(stem, oldPos, newPos, translation);
   res.set('Access-Control-Allow-Origin', '*');
   res.sendStatus(204);
   saveLexicalDatabase();
+  next();
 });
 
-app.post('/replaceTranslation', jsonParser, function (req, res) {
+app.post('/replaceTranslation', jsonParser, function (req, res, next) {
   const {stem, pos, oldTranslation, newTranslation} = req.body;
   lexicalDatabase.changeTranslation(stem, pos, oldTranslation, newTranslation);
   res.set('Access-Control-Allow-Origin', '*');
   res.sendStatus(204);
   saveLexicalDatabase();
+  next();
 });
 
-app.post('/addAttestation', jsonParser, function(req, res) {
+app.post('/addAttestation', jsonParser, function(req, res, next) {
   const { analysis, attestation } = req.body;
   lexicalDatabase.concordance.addAttestation(analysis, attestation);
   res.set('Access-Control-Allow-Origin', '*');
   res.sendStatus(204);
   saveLexicalDatabase();
+  next();
 });
 
-app.post('/removeAttestation', jsonParser, function(req, res) {
+app.post('/removeAttestation', jsonParser, function(req, res, next) {
   const { analysis, attestation } = req.body;
   lexicalDatabase.concordance.removeAttestation(analysis, attestation);
   res.set('Access-Control-Allow-Origin', '*');
   res.sendStatus(204);
   saveLexicalDatabase();
+  next();
 });
 
-app.post('/addLine', jsonParser, function(req, res) {
+app.post('/addLine', jsonParser, function(req, res, next) {
   const { attestation, line } = req.body;
   lexicalDatabase.corpus.addLine(attestation, line);
   res.set('Access-Control-Allow-Origin', '*');
   res.sendStatus(204);
   saveLexicalDatabase();
+  next();
 });
 
-app.post('/updateLine', jsonParser, function(req, res) {
+app.post('/updateLine', jsonParser, function(req, res, next) {
   const { attestation, position, word } = req.body;
   lexicalDatabase.corpus.updateLine(attestation, position, word);
   res.set('Access-Control-Allow-Origin', '*');
   res.sendStatus(204);
   saveLexicalDatabase();
+  next();
+});
+
+function removePrefix(prefix: string, text: string): string {
+  if (text.startsWith(prefix)) {
+    return text.substring(prefix.length);
+  } else {
+    return text;
+  }
+}
+
+app.post('/*path', jsonParser, function(req, res, next) {
+  const message = 'data: ' + JSON.stringify(req.body);
+  const event = `event: ${removePrefix('/', req.path)}\n${message}\n\n`;
+  console.log(`Sending to ${clients.length} clients:\n${event}`);
+  clients.forEach(client => client.write(event));
 });
 
 app.listen(PORT)
