6f609661ffe8d49a9f326de0b0358b16038f2a3c
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-09-26 14:35:04 +0300

    Additional testing

--- test/check.sh
+++ test/check.sh
@@ -1,5 +1,5 @@
 source curl_jsonlines.sh
-function check_stems() {
+function checkStem() {
   while read -r line; do
     local oldStem=$(echo $line | jq .oldStem | sed 's/"//g')
     local newStem=$(echo $line | jq .newStem | sed 's/"//g')
@@ -16,10 +16,44 @@ function check_stems() {
   done < "$2"
   return
 }
-function modify_stems() {
-  curl_jsonlines stemReplacements.jsonlines replaceStem
+function checkPos() {
+  while read -r line; do
+    local stem=$(echo $line | jq .stem | sed 's/"//g')
+    local oldPos=$(echo $line | jq .oldPos | sed 's/"//g')
+    local newPos=$(echo $line | jq .newPos | sed 's/"//g')
+    local translation=$(echo $line | jq .translation | sed 's/"//g')
+    local key=$stem,$newPos
+    echo "Looking up $key"
+    local result=$(jq ".glosses[\"$key\"][0]" < $1)
+    if [[ $result == "\"$translation\"" ]]; then
+      echo "success: $result"
+    else
+      echo "expected: $translation, found: $result"
+    fi
+  done < "$2"
+  return
 }
-dict_path=~/Downloads/Dictionary.json
-function test_stems() {
-  check_stems $dict_path stemReplacements.jsonlines
+function checkTranslation() {
+  while read -r line; do
+    local stem=$(echo $line | jq .stem | sed 's/"//g')
+    local pos=$(echo $line | jq .pos | sed 's/"//g')
+    local oldTranslation=$(echo $line | jq .oldTranslation | sed 's/"//g')
+    local newTranslation=$(echo $line | jq .newTranslation | sed 's/"//g')
+    local key=$stem,$pos
+    echo "Looking up $key"
+    local result=$(jq ".glosses[\"$key\"][0]" < $1)
+    if [[ $result == "\"$newTranslation\"" ]]; then
+      echo "success: $result"
+    else
+      echo "expected: $newTranslation, found: $result"
+    fi
+  done < "$2"
+  return
+}
+function modify() {
+  curl_jsonlines "$1Replacements.jsonlines" "replace$1"
+}
+dict=~/Downloads/Dictionary.json
+function check() {
+  check$1 $dict "$1Replacements.jsonlines"
 }
